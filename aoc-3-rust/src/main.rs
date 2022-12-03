2c8ec19b2901f702ebd34e62605f2c2566dbc385
Korzhavin Ivan <korjavin@gmail.com>
2022-12-03 18:22:01 +0300

    2

--- aoc-3-rust/src/main.rs
+++ aoc-3-rust/src/main.rs
@@ -1,39 +1,64 @@
 use std::io::{self, BufRead};
 
-fn split_string(mut s: String, byte_index: usize) -> (String, String)
-{
+fn split_string(mut s: String, byte_index: usize) -> (String, String) {
     let tail = s[byte_index..].into();
     s.truncate(byte_index);
     (s, tail)
 }
 
+fn priority(c: char) -> u32 {
+    let minus: u32 = 'a'.into(); //TODO const
+    let MINUS: u32 = 'A'.into(); //TODO const
+    let b: u32 = c.into();
+    let mut ord: u32 = 0;
+    if b > 96 {
+        ord = b - minus + 1;
+    } else {
+        ord = b - MINUS + 27;
+    }
+    return ord;
+}
+
+fn common(s1: &String, s2: &String) -> char {
+    let mut common = 'a';
+    for i in s1.chars() {
+        if s2.contains(i) {
+            common = i;
+            break;
+        }
+    }
+    return common;
+}
+
+fn common3(s1: &String, s2: &String, s3: &String) -> char {
+    let mut common = 'a';
+    for i in s1.chars() {
+        if s2.contains(i) {
+            if s3.contains(i) {
+                common = i;
+                break;
+            }
+        }
+    }
+    return common;
+}
 
 fn main() {
     let stdin = io::stdin();
     let mut sum = 0;
+    let mut counter =0;
+    let mut lines : [String; 3] =["".to_string(),"".to_string(),"".to_string()];
     for line in stdin.lock().lines() {
-        let val=line.unwrap();
-        let size=val.len();
-        let (part1,part2) = split_string(val,size/2 );
-        let mut common = 'a'  ;
-        let minus: u32 = 'a'.into();
-        let MINUS: u32 = 'A'.into();
-        for i in part1.chars() {
-            if part2.contains(i) {
-                common=i;
-                break;
-            }
+        let val = line.unwrap();
+        lines[counter]=val;
+        counter+=1;
+        if counter>2 {
+        let common = common3(&lines[0], &lines[1], &lines[2]);
+        let ord = priority(common);
+        print!("{}-{}-", common, ord);
+        sum += ord;
+        counter=0;
         }
-            let b: u32 = common.into();
-            let mut ord :u32 = 0;
-            if b>96 {
-                ord=b-minus+1;
-            } else {
-                ord=b-MINUS+27;
-            }
-            print!("{}-{}-",common,ord);
-            sum+=ord;
-        println!("{},{}",part1, part2);
     }
     println!("score: {}", sum)
 }
