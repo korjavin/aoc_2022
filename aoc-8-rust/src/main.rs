cef26f8ddb120de714351e250715b8516e101e51
Korzhavin Ivan <korjavin@gmail.com>
2022-12-08 17:43:01 +0300

    2

--- aoc-8-rust/src/main.rs
+++ aoc-8-rust/src/main.rs
@@ -15,6 +15,46 @@ fn is_bigger_than_any(number: usize, numbers: &Vec<usize>) -> bool {
     return number > max(numbers);
 }
 
+fn score_of_numbers(number: usize, numbers: &Vec<usize>) -> usize{
+    let mut score = 0;
+    for i in 0..numbers.len() {
+        score+=1;
+        if number<=numbers[i] {
+            return score;
+        }
+    }
+    score
+}
+
+fn count_score(x: usize, y: usize, map: &Vec<Vec<usize>>) -> usize {
+    if x <= 0 || y <= 0 || x == map[0].len()-1 || y == map.len()-1 {
+        return 0;
+    }
+    let val = map[x][y];
+    let mut vec = Vec::new();
+    for i in (0..x).rev()  {
+        vec.push(map[i][y]);
+    }
+    let top = score_of_numbers (val, &vec) ;
+    vec = Vec::new();
+    for i in x + 1..map[0].len() {
+        vec.push(map[i][y]);
+    }
+    let bottom  = score_of_numbers(val, &vec) ;
+    vec = Vec::new();
+    for i in y + 1..map.len() {
+        vec.push(map[x][i]);
+    }
+    let left = score_of_numbers(val, &vec) ;
+    vec = Vec::new();
+    for i in (0..y).rev()  {
+        vec.push(map[x][i]);
+    }
+    let right = score_of_numbers(val, &vec) ;
+
+    return top * bottom * left * right;
+}
+
 fn is_visible(x: usize, y: usize, map: &Vec<Vec<usize>>) -> bool {
     if x <= 0 || y <= 0 || x == map[0].len()-1 || y == map.len()-1 {
         return true;
@@ -56,8 +96,13 @@ fn main() {
     }
 
     let mut counter =0;
+    let mut max_score = 0;
     for i in 0..lines.len() {
         for j in 0..lines[i].len() {
+            let score = count_score(i, j, &lines);
+            if score > max_score {
+                max_score = score;
+            }
             if is_visible(j, i, &lines) {
                 print!("X");
                 counter+=1;
@@ -67,5 +112,5 @@ fn main() {
         }
         println!("");
     }
-    println!("sum visible = {}", counter);
+    println!("sum visible = {}, score = {}", counter, max_score);
 }
